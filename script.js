544337ec7da38bb36f2ac4789f6b4eca0466e0e2
Aryaa007 <aryaparihar0512@gmail.com>
2025-06-24 15:41:21 +0530

    Changes

--- script.js
+++ script.js
@@ -12,15 +12,14 @@ const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 const N=1;
 const cars=generateCars(N);
 let bestCar=cars[0];
-if(localStorage.getItem("bestBrain")){
-    for(let i=0;i<cars.length;i++){
-        cars[i].brain=JSON.parse(
-            localStorage.getItem("bestBrain"));
-        if(i!=0){
-            NeuralNetwork.mutate(cars[i].brain,0.1);
-        }
+const bestBrain = localStorage.getItem("bestBrain");
+
+if (bestBrain) {
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].brain = JSON.parse(bestBrain);
     }
 }
+
 
 let traffic = [];
 const laneCount = 3;
